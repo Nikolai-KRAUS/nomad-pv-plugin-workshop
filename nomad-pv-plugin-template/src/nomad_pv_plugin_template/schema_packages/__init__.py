bc0e12a139633382a4a11c521b5dac6a3879c75a
Nikolai KRAUS <nikolai.kraus@uibk.ac.at>
2025-07-07 15:33:42 +0000

    bla

---
+++ nomad-pv-plugin-template/src/nomad_pv_plugin_template/schema_packages/__init__.py
@@ -0,0 +1,32 @@
+from nomad.config.models.plugins import SchemaPackageEntryPoint
+from pydantic import Field
+
+
+class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from nomad_pv_plugin_template.schema_packages.schema_package import m_package
+
+        return m_package
+
+
+class fooPackageEntryPoint(SchemaPackageEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from nomad_pv_plugin_template.schema_packages.foo_package import m_package
+
+        return m_package
+
+
+schema_package_entry_point = NewSchemaPackageEntryPoint(
+    name='NewSchemaPackage',
+    description='New schema package entry point configuration.',
+)
+
+
+foo_schema_package_entry_point = fooPackageEntryPoint(
+    name='fooPackage',
+    description='foo package entry point configuration.',
+)
